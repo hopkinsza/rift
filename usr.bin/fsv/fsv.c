2827d47af6c6ed031438d35ba8f9715b5ca42d0a
Zach Hopkins <hopkinsza@gmail.com>
2021-04-04 12:38:26 -0400

    function to make dirs; comments for brainstorming flags

--- usr.bin/fsv/fsv.c
+++ usr.bin/fsv/fsv.c
@@ -1,7 +1,12 @@
+//#define _GNU_SOURCE
+
+#include <sys/stat.h>
 #include <sys/time.h>
 #include <sys/wait.h>
 
 #include <err.h>
+#include <errno.h>
+#include <fcntl.h>
 #include <getopt.h>
 #include <signal.h>
 #include <stdio.h>
@@ -9,6 +14,30 @@
 #include <string.h>
 #include <sysexits.h>
 #include <unistd.h>
+
+/*
+ * -n name
+ */
+
+/*
+ * chpst opts:
+ * -b argv0
+ * -e envdir
+ * -/ root
+ * -n nice
+ * -l lock
+ *   -m bytes: memory limits
+ *   -d bytes: data segment limit
+ *   -o n: open fd limit
+ *   -p n: process limit per uid
+ *   -f bytes: output file size limit
+ *   -c bytes: coredump size limit
+ * -v
+ * -P new process group
+ * -0 close stdin
+ * -1 close stdout
+ * -2 close stderr
+ */
 
 static const char *progversion = "0.0.0";
 static char *progname;
@@ -47,6 +76,68 @@ void
 onterm(int sig)
 {
 	termsig = sig;
+}
+
+/*
+ * This function essentially does the following:
+ * mkdir ${prefix}/fsv-${uid}
+ * mkdir ${prefix}/fsv-${uid}/${cmddir}
+ *
+ * Returns fd to the new cmddir.
+ */
+int
+mkcmddir(char *cmddir, char *prefix)
+{
+	int fd_prefix;
+	int fd_fsvdir;
+	int fd_cmddir;
+	char fsvdir[32];
+
+	/* build name of fsvdir */
+	int l;
+	l = snprintf(fsvdir, sizeof(fsvdir), "fsv-%ld", (long)geteuid());
+	if (l < 0) {
+		err(EX_OSERR, "snprintf");
+	} else if (l >= sizeof(fsvdir)) {
+		/* should never happen */
+		errx(EX_SOFTWARE, "snprintf: fsvdir too long");
+	}
+
+	/* open $prefix */
+	if ((fd_prefix = open(prefix, O_RDONLY|O_DIRECTORY)) == -1)
+		err(EX_OSFILE, "can't open `%s'", prefix);
+
+	/* mkdir $prefix/fsv-$uid */
+	if (mkdirat(fd_prefix, fsvdir, 00777) == -1) {
+		switch (errno) {
+		case EEXIST:
+			/* already exists, ok */
+			break;
+		default:
+			err(EX_IOERR, "can't mkdir `%s/%s'", prefix, fsvdir);
+		}
+	}
+	if ((fd_fsvdir = openat(fd_prefix, fsvdir, O_RDONLY|O_DIRECTORY)) == -1)
+		err(EX_IOERR, "can't open `%s/%s' as directory", prefix, fsvdir);
+
+	close(fd_prefix);
+
+	/* mkdir $prefix/fsv-$uid/$cmddir */
+	if ((mkdirat(fd_fsvdir, cmddir, 00777)) == -1) {
+		switch (errno) {
+		case EEXIST:
+			/* already exists, ok */
+			break;
+		default:
+			err(EX_IOERR, "can't mkdir `%s/%s/%s'", prefix, fsvdir, cmddir);
+		}
+	}
+	if ((fd_cmddir = openat(fd_fsvdir, cmddir, O_RDONLY|O_DIRECTORY)) == -1)
+		err(EX_IOERR, "can't open `%s/%s/%s' as directory", prefix, fsvdir, prefix);
+
+	close(fd_fsvdir);
+
+	return fd_cmddir;
 }
 
 int
@@ -118,6 +209,8 @@ main(int argc, char *argv[])
 	}
 	argc -= optind;
 	argv += optind;
+
+	/* TODO call mkdir func */
 
 	/*
 	 * Exec given cmd.
