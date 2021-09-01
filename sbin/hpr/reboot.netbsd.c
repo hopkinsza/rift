e0de56775ad3d53f6da3e2337fbc668fc8bd5fa8
zach hopkins <hopkinsza@gmail.com>
2021-09-01 06:17:55 -0400

    add reboot.netbsd.c

---
+++ sbin/hpr/reboot.netbsd.c
@@ -0,0 +1,25 @@
+/*
+ * SPDX-License-Identifier: 0BSD
+ */
+
+#include <sys/reboot.h>
+
+#include <errno.h>
+#include <unistd.h>
+
+#include "osind_reboot.h"
+
+int
+osind_reboot(int howto)
+{
+	if (howto == OSIND_RB_HALT) {
+		reboot(RB_HALT, NULL);
+	} else if (howto == OSIND_RB_POWEROFF) {
+		reboot(RB_HALT|RB_POWERDOWN, NULL);
+	} else if (howto == OSIND_RB_POWEROFF) {
+		reboot(RB_AUTOBOOT, NULL);
+	} else {
+		errno = EINVAL;
+	}
+	return -1;
+}
