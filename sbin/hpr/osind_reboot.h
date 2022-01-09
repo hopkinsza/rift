ae9cc5adffc43ee05059bc8835ca41df342c74ef
zach hopkins <hopkinsza@gmail.com>
2022-01-09 09:32:48 -0600

    change #defines to keep consistent order: hpr

--- sbin/hpr/osind_reboot.h
+++ sbin/hpr/osind_reboot.h
@@ -6,8 +6,8 @@
 #define _OSIND_REBOOT_H_
 
 #define OSIND_RB_HALT     0
-#define OSIND_RB_REBOOT   1
-#define OSIND_RB_POWEROFF 2
+#define OSIND_RB_POWEROFF 1
+#define OSIND_RB_REBOOT   2
 
 /*
  * One function, an OS independent wrapper around reboot(2).
