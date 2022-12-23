5922ce139b4c36c254869ae08d61169ca1a9b73e
Zach Hopkins <hopkinsza@gmail.com>
2022-12-23 13:21:51 -0600

    rewrite init

--- sbin/init/init.c
+++ sbin/init/init.c
@@ -1,27 +1,5 @@
 /*
  * SPDX-License-Identifier: 0BSD
- *
- * We are always in one of two states: SINGLEUSER or MULTIUSER.
- *   SINGLEUSER -> an emergency shell /bin/sh is running.
- *   MULTIUSER  -> /etc/rc is running. This is the default unless
- *                 argv[1] == "-s".
- * Either way, we keep track of the PID of that process in `cpid'.
- *
- * If the `cpid' process exits, switch state to the opposite mode.
- * The exception to this is if it has been terminated by SIGTERM or SIGHUP.
- * In this case, assume that this was done by halt/poweroff/reboot and don't
- * do anything.
- *
- * Only respond to the following signals:
- *   SIGALRM || SIGCHLD -> wait() on any children. A SIGALRM is scheduled to be
- *                         sent every 30 seconds, to make absolutely certain
- *                         that every child process is wait()ed on.
- *   SIGINT  || SIGTERM -> If we are in MULTIUSER state, send signals TERM,
- *                         HUP, and CONT to every process in the system.
- *                         Wait a bit for them to exit cleanly, then KILL any
- *                         processes that remain. (If any process remains alive
- *                         after this, print a warning.) Then switch to
- *                         SINGLEUSER state.
  */
 
 #include <sys/ioctl.h>
@@ -33,7 +11,6 @@
 #include <fcntl.h>
 #include <paths.h>
 #include <signal.h>
-#include <stdarg.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -42,258 +19,237 @@
 
 #include "rfconf.h"
 
-#include "debug.h"
-
-#define INIT_CONSOLE "/dev/console"
-#define INIT_PATH    _PATH_STDPATH
-
 #define INIT_RC_PATH ETCDIR "/rc"
 #define INIT_RC_NAME "rc"
-#define INIT_SHELL_PATH "/bin/sh"
-#define INIT_SHELL_NAME "-sh"
+
+#define INIT_SINGLE_PATH ETCDIR "/rc.single"
+#define INIT_SINGLE_NAME "rc.single"
 
 enum states {
-	SINGLEUSER,
-	MULTIUSER
+	MULTIUSER,
+	SINGLEUSER
 } state;
 
-int  fork_rc();
-int  fork_shell();
-int  wait_for_upto(int);
-void setctty();
-void sleep_err(int, char *, ...);
-void start_timer();
-void swap_state(int *, enum states *);
+int spawn_for(enum states);
+int wait_for_upto(int);
+void hang();
+void init_child();
+void reset_procs();
 
-const struct timespec one_sec = {
-	1,
-	0
-};
+int fd_console;
+pid_t cpid;	// pid of rc or single user shell
+sigset_t bmask;	// signal block mask -- full
 
-/* signal block mask -- full */
-sigset_t bmask;
-/* fd for /dev/console */
-int cons_fd;
+const struct timespec one_sec = { 1, 0 };
 
 int
 main(int argc, char *argv[])
 {
-	int sig;
-	int status;
-	pid_t cpid;
-	pid_t pid;
-
 	if (getpid() != 1)
 		return 1;
-	if (getuid() != 0)
-		return 1;
 
 	/*
-	 * Open fd for /dev/console and make sure STD{IN,OUT,ERR} use it.
+	 * Initial setup.
 	 */
 
-	cons_fd = open(INIT_CONSOLE, O_RDWR|O_NOCTTY);
-	if (cons_fd == -1)
-		sleep_err(1, "open /dev/console failed");
+	chdir("/");
 
-	dup2(cons_fd, 0);
-	dup2(cons_fd, 1);
-	dup2(cons_fd, 2);
+	// Block all signals.
+	sigfillset(&bmask);
+	sigprocmask(SIG_BLOCK, &bmask, NULL);
 
-	/*
-	 * Call setsid() to create initial session.
-	 * Set a default PATH and umask.
-	 */
-
+	// Create initial session, set default PATH and umask.
 	setsid();
 	setenv("PATH", _PATH_STDPATH, 1);
 	umask(0022);
 
+	// Open fd for /dev/console and make sure STD{IN,OUT,ERR} use it.
+	fd_console = open("/dev/console", O_RDWR|O_NOCTTY);
+	if (fd_console == -1) {
+		warn("open /dev/console failed");
+		hang();
+	}
+	dup2(fd_console, 0);
+	dup2(fd_console, 1);
+	dup2(fd_console, 2);
+
 	/*
-	 * Block all signals until ready to handle them.
+	 * Process flags, then spawn the first process.
 	 */
 
-	sigfillset(&bmask);
-	sigprocmask(SIG_BLOCK, &bmask, NULL);
+	state = MULTIUSER;
 
-	/* Set initial state. */
-	state = SINGLEUSER;
-	if (argc == 2 && (strncmp(argv[1], "-s", 2)) == 0) {
-		debug("got -s\n");
-		state = MULTIUSER;
+	int ch;
+	while ((ch = getopt(argc, argv, "s")) != -1) {
+		switch (ch) {
+		case 's':
+			state = SINGLEUSER;
+			break;
+		default:
+			warnx("unrecognized flag: -%c", ch);
+			break;
+		}
 	}
-	swap_state(&cpid, &state);
 
-	start_timer();
+	cpid = spawn_for(state);
 
-	while (sigwait(&bmask, &sig) == 0)
-	switch (sig) {
+	/*
+	 * Main loop.
+	 */
+
+	// Set an alarm to catch corner-cases.
+	// For example, a process could exit before waiting on its children,
+	// then those zombie children are re-parented to init.
+	alarm(90);
+
+	int do_respawn = 1;
+
+	int sig;
+	while (sigwait(&bmask, &sig) == 0) switch (sig) {
 	case SIGALRM:
 	case SIGCHLD:
-		if (DEBUG) {
-			if (sig == SIGALRM)
-				debug("got alrm\n");
-			if (sig == SIGCHLD)
-				debug("got chld\n");
-		}
+	{
+		int status;
+		pid_t pid;
 
+		alarm(90);
+
+		// wait on all possible children
 		while ((pid = waitpid(-1, &status, WNOHANG)) > 0) {
-			if (pid == cpid) {
-				/*
-				 * Most programs should behave as expected with
-				 * the W* macros described in wait(2).
-				 */
-				if (WIFSIGNALED(status) &&
-				    (WTERMSIG(status) == SIGTERM ||
-				     WTERMSIG(status) == SIGHUP)) {
-					debug("cpid terminated by TERM or HUP\n");
-					cpid = 0;
-				}
-				/*
-				 * But some programs exit with 128+signumber.
-				 */
-				else if (WIFEXITED(status) &&
-				         (WEXITSTATUS(status) == 129 ||
-					  WEXITSTATUS(status) == 143)) {
-					debug("cpid exited with status 129 or 143");
-					cpid = 0;
-				} else {
-					debug("cpid exited, running swap_state()\n");
-					swap_state(&cpid, &state);
-				}
+			if (pid == cpid && do_respawn) {
+				raise(SIGTERM);
 			}
 		}
 		break;
-	case SIGINT:
+	}
 	case SIGTERM:
-		debug("got int or term\n");
-		if (state == SINGLEUSER)
-			break;
+		if (state == MULTIUSER) {
+			state = SINGLEUSER;
+			warnx("entering singleuser mode");
+		} else if (state == SINGLEUSER) {
+			state = MULTIUSER;
+			warnx("entering multiuser mode");
+		}
 
-		swap_state(&cpid, &state);
-
+		reset_procs();
+		cpid = spawn_for(state);
+		break;
+	case SIGUSR1:
+		do_respawn = 0;
+		break;
+	case SIGUSR2:
+		do_respawn = 1;
 		break;
 	}
 }
 
+/*
+ * Spawn /etc/rc for MULTIUSER mode, or /etc/rc.single for SINGLEUSER mode.
+ */
 int
-fork_rc()
+spawn_for(enum states s)
 {
-	int pid;
-	if ((pid = fork()) == 0) {
-		sigprocmask(SIG_UNBLOCK, &bmask, NULL);
-		setctty();
-		execl(INIT_RC_PATH, INIT_RC_NAME, NULL);
+	int pid = fork();
+
+	if (pid == -1) {
+		warn("fork failed");
+		hang();
 	}
+
+	if (pid == 0) {
+		init_child();
+
+		if (s == MULTIUSER) {
+			execl(INIT_RC_PATH, INIT_RC_NAME, NULL);
+			warn("(child) exec " INIT_RC_PATH " failed");
+			exit(1);
+		} else if (s == SINGLEUSER) {
+			execl(INIT_SINGLE_PATH, INIT_SINGLE_NAME, NULL);
+			// fallback to a shell
+			execl("/bin/sh", "-sh", NULL);
+
+			// If you don't have either of those, you are seriously screwed.
+			// Tell init not to bother re-execing anymore.
+			kill(1, SIGUSR1);
+			warn("(child) could not exec " INIT_SINGLE_PATH " or /bin/sh");
+			fprintf(stderr, "\n");
+			fprintf(stderr, "your system is screwed;\n");
+			fprintf(stderr, "you will need to perform a daring rescue or reinstall");
+			exit(1);
+		}
+	}
+
 	return pid;
 }
 
-int
-fork_shell()
-{
-	int pid;
-	if ((pid = fork()) == 0) {
-		sigprocmask(SIG_UNBLOCK, &bmask, NULL);
-		setctty();
-		execl(INIT_SHELL_PATH, INIT_SHELL_NAME, NULL);
-	}
-	return pid;
-}
-
+/*
+ * Wait for all existing processes to exit, with a timeout.
+ * Return 0 if everything has exited, otherwise 1.
+ */
 int
 wait_for_upto(int secs)
 {
 	int pid;
-	int status;
 
 	for (int i=0; i<secs; i++) {
-		while ((pid = waitpid(-1, &status, WNOHANG)) > 0)
+		while ((pid = waitpid(-1, NULL, WNOHANG)) > 0)
 			;
 
-		if (pid == -1) {
-			/* All children successfully waited for. */
+		if (pid == -1 && errno == ECHILD) {
+			// All children successfully waited for.
 			return 0;
 		}
 
 		nanosleep(&one_sec, NULL);
 	}
 
-	/* Unwaited children remain. */
+	// Unwaited children remain.
 	return 1;
 }
 
 /*
- * Called by child after forking.
- * Create a new session and make /dev/console the ctty.
+ * Sleep for a small bit, then exit, so a user can read an error message.
  */
 void
-setctty()
+hang()
 {
-	if (setsid() == -1)
-		warnx("setsid(2) failed");
-	if (ioctl(cons_fd, TIOCSCTTY) == -1)
-		warnx("ioctl to make /dev/console my ctty failed");
-}
-
-/*
- * Print error, sleep for a few seconds, then exit.
- */
-void
-sleep_err(int status, char *fmt, ...)
-{
-	va_list ap;
-
-	fprintf(stderr, "\n");
-
-	va_start(ap, fmt);
-	vwarn(fmt, ap);
-	va_end(ap);
-
 	for (int i=0; i<6; i++) {
 		nanosleep(&one_sec, NULL);
 	}
 
-	exit(status);
+	exit(1);
 }
 
+/*
+ * Initialization to be run in a child process.
+ */
 void
-start_timer()
+init_child()
 {
-	timer_t tid;
-	struct itimerspec its;
-
-	timer_create(CLOCK_MONOTONIC, NULL, &tid);
-
-	its = (struct itimerspec){
-		{ 30, 0 },
-		{ 30, 0 }
-	};
-	timer_settime(tid, TIMER_ABSTIME, &its, NULL);
+	sigprocmask(SIG_UNBLOCK, &bmask, NULL);
+	if (setsid() == -1)
+		warn("(child) setsid(2) failed");
+	if (ioctl(fd_console, TIOCSCTTY, 1) == -1)
+		warn("(child) ioctl to make /dev/console the ctty failed");
 }
 
+/*
+ * Terminate all existing processes.
+ */
 void
-swap_state(int *cpid, enum states *state)
+reset_procs()
 {
-	if (*state == MULTIUSER) {
-		warnx("entering single-user mode");
-		warnx("waiting up to 15 seconds for processes to exit...");
-		kill(-1, SIGTERM);
-		kill(-1, SIGHUP);
-		kill(-1, SIGCONT);
-		if (wait_for_upto(10) != 0) {
-			kill(-1, SIGKILL);
-			if (wait_for_upto(5) != 0) {
-				warnx("some processes would not die; ps axl advised");
-			}
-		}
-		*state = SINGLEUSER;
-		*cpid = fork_shell();
-	} else if (*state == SINGLEUSER) {
-		warnx("entering multi-user mode");
-		*state = MULTIUSER;
-		*cpid = fork_rc();
-	}
+	warnx("waiting up to 10 seconds for processes to exit...");
 
-	if (*cpid == -1)
-		sleep_err(1, "fork(2) failed");
+	kill(-1, SIGTERM);
+	kill(-1, SIGHUP);
+	kill(-1, SIGCONT);
+	if (wait_for_upto(8) == 0)
+		return;
+
+	kill(-1, SIGKILL);
+	if (wait_for_upto(2) == 0)
+		return;
+
+	warnx("some processes would not die; ps axl advised");
 }
