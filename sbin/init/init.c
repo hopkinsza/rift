82d93b1461452e9cf80e119e899d1c8f309115ea
Zach Hopkins <hopkinsza@gmail.com>
2025-07-24 17:01:18 -0400

    complete reimplementation

--- sbin/init/init.c
+++ sbin/init/init.c
@@ -2,10 +2,13 @@
  * SPDX-License-Identifier: 0BSD
  */
 
+#include <sys/ioctl.h>
 #include <sys/param.h>
 #include <sys/stat.h>
+#include <sys/time.h>
 #include <sys/wait.h>
 
+#include <err.h>
 #include <errno.h>
 #include <fcntl.h>
 #include <paths.h>
@@ -14,105 +17,128 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <syslog.h>
 #include <time.h>
 #include <unistd.h>
 
-#if defined(BSD)
-#include <util.h>
-#endif
-
-#if defined(__linux__)
-// glibc has login_tty(3) in utmp.h
-#include <utmp.h>
-#endif
-
 #include "rfconf.h"
 
-#define INIT_RC_PATH ETCDIR "/rc"
-#define INIT_RC_NAME "rc"
+#define RC_PATH ETCDIR "/rc"
+#define RC_NAME "rc"
+#define RC_SINGLE_PATH ETCDIR "/rc.single"
+#define RC_SINGLE_NAME "rc.single"
+#define RC_INT_PATH ETCDIR "/rc.int"
+#define RC_INT_NAME "rc.int"
 
-#define INIT_SINGLE_PATH ETCDIR "/rc.single"
-#define INIT_SINGLE_NAME "rc.single"
+enum modes { SINGLE, MULTI };
 
-enum states {
-	MULTIUSER,
-	SINGLEUSER
-} state;
+pid_t clean_fork();
+pid_t spawn_rc(enum modes, int);
+int spawn_rc_int();
+int wait_while_procs(int);
+void smite();
 
-int spawn_for(enum states);
-int wait_for_upto(int);
-void hang();
-void reset_procs();
-void w(const char *, ...);
-void wx(const char *, ...);
+void notice (const char *, ...);
+void warning(const char *, ...);
+void error  (const char *, ...);
+void l(int,  const char *, va_list);
 
-pid_t cpid;	// pid of rc or single user shell
-sigset_t bmask;	// signal block mask -- full
-
-const struct timespec one_sec = { 1, 0 };
-
-void
-nop(int i)
-{
-}
+void nop(int i) {}
 
 int
 main(int argc, char *argv[])
 {
-	if (getpid() != 1)
-		return 1;
-
 	/*
-	 * Initial setup.
+	 * Be amicable.
 	 */
 
-	// Block all signals.
-	sigfillset(&bmask);
-	sigprocmask(SIG_BLOCK, &bmask, NULL);
+	if (getpid() != 1)
+		errx(1, "already running");
 
-	// For all signals we deal with, set its handler to a no-op function.
-	// On BSD, it seems a signal will not be posted to us if it's ignored,
-	// even if it is blocked.
-	{
-		struct sigaction sa;
-		memset(&sa, 0, sizeof(sa));
+	if (geteuid() != 0)
+		errx(1, "must run as root");
 
-		sa.sa_flags = SA_NOCLDSTOP;
-		sa.sa_handler = nop;
-		sigaction(SIGALRM, &sa, NULL);
-		sigaction(SIGCHLD, &sa, NULL);
-		sigaction(SIGTERM, &sa, NULL);
-		sigaction(SIGTSTP, &sa, NULL);
-		sigaction(SIGHUP, &sa, NULL);
-	}
+	/*
+	 * Initialize.
+	 */
 
-	// Create initial session.
+	// paranoia
+	close(0);
+	close(1);
+	close(2);
+{
+	int fd;
+	fd = open(_PATH_DEVNULL, O_RDWR|O_NOCTTY);
+	dup2(fd, 0);
+	dup2(fd, 1);
+	dup2(fd, 2);
+	if (fd > 2)
+		close(fd);
+}
+
+	// create initial session
 	setsid();
+	// establish initial user (a no-op on some systems)
+	setlogin("root");
 
-	// Other random setup.
 	chdir("/");
 	setenv("PATH", _PATH_STDPATH, 1);
 	umask(0022);
 
+	openlog("init", 0, LOG_AUTH);
+
 	/*
-	 * Process flags, then spawn the first process.
+	 * Process flags.
 	 */
 
-	state = MULTIUSER;
-
+	enum modes mode = MULTI;
+{
 	int ch;
-	while ((ch = getopt(argc, argv, "s")) != -1) {
+	while((ch = getopt(argc, argv, "s")) != -1) {
 		switch (ch) {
 		case 's':
-			state = SINGLEUSER;
+			mode = SINGLE;
 			break;
 		default:
-			wx("unrecognized flag: -%c", ch);
+			warning("unrecognized flag: -%c", ch);
 			break;
 		}
 	}
 
-	cpid = spawn_for(state);
+	if (optind != argc)
+		notice("ignoring excess arguments");
+}
+
+	/*
+	 * Block signals.
+	 */
+
+	// Set handlers to a no-op function;
+	// this ensures they are not ignored.
+	// An ignored signal (SIG_IGN) may not be posted at all.
+	// These will reset to SIG_DFL on exec.
+{
+	struct sigaction sa;
+	memset(&sa, 0, sizeof(sa));
+	sa.sa_handler = nop;
+	sigaction(SIGALRM, &sa, NULL);
+	sigaction(SIGCHLD, &sa, NULL);
+	sigaction(SIGINT,  &sa, NULL);
+	sigaction(SIGTERM, &sa, NULL);
+	sigaction(SIGTSTP, &sa, NULL);
+	sigaction(SIGHUP,  &sa, NULL);
+}
+
+	// block
+	sigset_t bmask;
+	sigemptyset(&bmask);
+	sigaddset(&bmask, SIGALRM);
+	sigaddset(&bmask, SIGCHLD);
+	sigaddset(&bmask, SIGINT);
+	sigaddset(&bmask, SIGTERM);
+	sigaddset(&bmask, SIGTSTP);
+	sigaddset(&bmask, SIGHUP);
+	sigprocmask(SIG_BLOCK, &bmask, NULL);
 
 	/*
 	 * Main loop.
@@ -121,10 +147,14 @@ main(int argc, char *argv[])
 	// Set an alarm to catch corner-cases.
 	// For example, a process could exit before waiting on its children,
 	// then those zombie children are re-parented to init.
-	alarm(90);
+{
+	struct itimerval it = { {60, 0}, {60, 0}};
+	setitimer(ITIMER_REAL, &it, NULL);
+}
 
-	int do_respawn = 1;
+	pid_t rc = spawn_rc(mode, 0);
 
+	int death = 0;
 	int sig;
 	while (sigwait(&bmask, &sig) == 0) switch (sig) {
 	case SIGALRM:
@@ -133,179 +163,316 @@ main(int argc, char *argv[])
 		int status;
 		pid_t pid;
 
-		alarm(90);
-
 		// wait on all possible children
-		while ((pid = waitpid(-1, &status, WNOHANG)) > 0) {
-			if (pid == cpid && do_respawn) {
-				raise(SIGTERM);
+		while ((pid = waitpid(-1, &status, WNOHANG|WUNTRACED)) > 0) {
+			if (pid != rc)
+				continue;
+
+			if (WIFSTOPPED(status)) {
+				notice("rc stopped, sending SIGCONT");
+				kill(pid, SIGCONT);
+				continue;
 			}
+
+			// rc has exited.
+			// Avoid possible collisions with other PIDs
+			// and waitpid(2)'s return values.
+			rc = -2;
+
+			if (death)
+				continue;
+
+			// Multiuser rc exited 0, ignore.
+			if (mode == MULTI &&
+			    WIFEXITED(status) && WEXITSTATUS(status) == 0) {
+					continue;
+			}
+
+			// Swap mode.
+			if (mode == SINGLE)
+				mode = MULTI;
+			else
+				mode = SINGLE;
+			rc = spawn_rc(mode, 1);
 		}
 		break;
 	}
-	case SIGTERM:
-		if (state == MULTIUSER) {
-			state = SINGLEUSER;
-			wx("entering singleuser mode");
-		} else if (state == SINGLEUSER) {
-			state = MULTIUSER;
-			wx("entering multiuser mode");
+	case SIGINT:
+		notice("got SIGINT");
+		if (spawn_rc_int() == -1) {
+			mode = SINGLE;
+			rc = spawn_rc(mode, 1);
 		}
-
-		reset_procs();
-		cpid = spawn_for(state);
+		break;
+	case SIGTERM:
+		notice("got SIGTERM");
+		if (death) {
+			break;
+		} else {
+			mode = SINGLE;
+			rc = spawn_rc(mode, 1);
+		}
 		break;
 	case SIGTSTP:
-		do_respawn = 0;
+		notice("got SIGTSTP");
+		death = 1;
 		break;
 	case SIGHUP:
-		do_respawn = 1;
+		notice("got SIGHUP");
+		death = 0;
+		break;
+	}
+
+	// unreachable
+	abort();
+}
+
+/*
+ * Wrapper around fork(2) with signals unblocked in child.
+ * Also clean up a possible stray syslog(3) fd.
+ */
+pid_t
+clean_fork()
+{
+	pid_t pid = fork();
+
+	switch (pid) {
+	case -1:
+		return -1;
+	case 0:
+	{
+		// child
+
+		// unblock signals; handlers will be restored on exec
+		sigset_t bmask;
+		sigemptyset(&bmask);
+		sigprocmask(SIG_SETMASK, &bmask, NULL);
+
+		closelog();
+
+		return 0;
+		break;
+	}
+	default:
+		// parent
+		return pid;
 		break;
 	}
 }
 
 /*
- * Spawn /etc/rc for MULTIUSER mode, or /etc/rc.single for SINGLEUSER mode.
+ * Spawn /etc/rc for multi-user mode, or /etc/rc.single for single-user mode.
  */
-int
-spawn_for(enum states s)
+pid_t
+spawn_rc(enum modes mode, int do_smite)
 {
-	int pid = fork();
+	if (mode == SINGLE)
+		notice("entering single-user mode");
+	else
+		notice("entering multi-user mode");
 
-	if (pid == -1) {
-		w("fork failed");
-		hang();
+	if (do_smite)
+		smite();
+
+	pid_t pid = clean_fork();
+	// If the fork(2) fails, keep trying ad infinitum.
+	while (pid == -1) {
+		error("fork() failed: %s", strerror(errno));
+		notice("trying again in 5 seconds...");
+		nanosleep(&(struct timespec){5,0}, NULL);
+		pid = clean_fork();
 	}
 
-	if (pid == 0) {
-		// Unblock signals.
-		sigprocmask(SIG_UNBLOCK, &bmask, NULL);
+	switch (pid) {
+	case 0:
+	{
+		// child
 
-		// Call login_tty(3) on constty or console.
-		// It will call setsid(2), set up STD{IN,OUT,ERR},
-		// set the controlling tty, and then close fd if needed.
+		// Set up a new session with controlling terminal.
+		// There are 2 historical implementations for how a
+		// session leader acquires a controlling terminal:
+		// 1. the sysv way:
+		//    - open(2) a tty file without O_NOCTTY in the flags
+		// 2. the bsd way:
+		//    - use ioctl(2) with TIOCSCTTY on a tty file descriptor
+		//    - or better yet, use wrapper function login_tty(3)
+		//
+		// This works for either, essentially replicating login_tty(3).
+
+		setsid();
+
 		int fd;
+		// NetBSD distinguishes between /dev/console for log messages
+		// and /dev/constty for the console tty.
 #if defined(_PATH_CONSTTY)
-		fd = open(_PATH_CONSTTY, O_RDWR|O_NOCTTY);
+		fd = open(_PATH_CONSTTY, O_RDWR);
 #else
-		fd = open(_PATH_CONSOLE, O_RDWR|O_NOCTTY);
+		fd = open(_PATH_CONSOLE, O_RDWR);
 #endif
-		login_tty(fd);
 
-		if (s == MULTIUSER) {
-			execl(INIT_RC_PATH, INIT_RC_NAME, NULL);
-			w("(child) exec " INIT_RC_PATH " failed");
+#if defined(TIOCSCTTY)
+		ioctl(fd, TIOCSCTTY, NULL);
+#endif
+
+		dup2(fd, 0);
+		dup2(fd, 1);
+		dup2(fd, 2);
+		if (fd > 2)
+			close(fd);
+
+		if (mode == MULTI) {
+			execl(RC_PATH, RC_NAME, NULL);
+			error("child: exec " RC_PATH " failed: %s",
+			      strerror(errno));
 			exit(1);
-		} else if (s == SINGLEUSER) {
-			execl(INIT_SINGLE_PATH, INIT_SINGLE_NAME, NULL);
-			w("(child) exec " INIT_SINGLE_PATH " failed");
-			wx("(child) falling back to /bin/sh");
+		} else {
+			execl(RC_SINGLE_PATH, RC_SINGLE_NAME, NULL);
+			error("child: exec " RC_SINGLE_PATH " failed: %s",
+			      strerror(errno));
+
+			notice("child: trying /bin/sh");
+			// don't leak syslog fd
+			closelog();
 			execl("/bin/sh", "-sh", NULL);
 
-			// If you don't have either of those, you are seriously screwed.
-			// Tell init not to bother re-execing anymore.
+			// tell init to not bother respawning
 			kill(1, SIGTSTP);
-			w("(child) could not exec " INIT_SINGLE_PATH " or /bin/sh");
-			fprintf(stderr, "\n");
-			fprintf(stderr, "your system is screwed;\n");
-			fprintf(stderr, "you will need to perform a daring rescue or reinstall");
+			warnx("child: could not exec " RC_SINGLE_PATH
+			      " or /bin/sh");
+			nanosleep(&(struct timespec){5,0}, NULL);
 			exit(1);
 		}
+		break;
 	}
-
-	return pid;
+	default:
+		// parent
+		return pid;
+		break;
+	}
 }
 
 /*
- * Wait for all existing processes to exit, with a timeout.
- * Return 0 if everything has exited, otherwise 1.
+ * Spawn /etc/rc.int.
  */
 int
-wait_for_upto(int secs)
+spawn_rc_int()
 {
-	int pid;
+	pid_t pid = clean_fork();
+	// If the fork(2) fails, retry once.
+	if (pid == -1) {
+		error("fork() for SIGINT failed: %s", strerror(errno));
+		notice("trying again in 1 second...");
+		nanosleep(&(struct timespec){1,0}, NULL);
+		pid = clean_fork();
+	}
 
-	for (int i=0; i<secs; i++) {
+	switch (pid) {
+	case -1:
+		error("fork() for SIGINT failed again: %s", strerror(errno));
+		return -1;
+		break;
+	case 0:
+		// child
+		execl(RC_INT_PATH, RC_INT_NAME, NULL);
+		error("child: exec " RC_INT_PATH " failed: %s",
+		      strerror(errno));
+		notice("child: telling init to go single-user");
+		kill(1, SIGTERM);
+		exit(1);
+		break;
+	default:
+		// parent
+		return pid;
+		break;
+	}
+}
+
+/*
+ * wait(2) on all child processes, up to a timeout.
+ */
+int
+wait_while_procs(int timeout)
+{
+	for (int i=0; i<timeout; i++) {
+		int pid;
+
 		while ((pid = waitpid(-1, NULL, WNOHANG)) > 0)
 			;
 
 		if (pid == -1 && errno == ECHILD) {
-			// All children successfully waited for.
+			// no children remain
 			return 0;
 		}
 
-		nanosleep(&one_sec, NULL);
+		// children remain
+		nanosleep(&(struct timespec){1,0}, NULL);
 	}
 
-	// Unwaited children remain.
-	return 1;
-}
-
-/*
- * Sleep for a small bit, then exit, so a user can read an error message.
- */
-void
-hang()
-{
-	for (int i=0; i<6; i++) {
-		nanosleep(&one_sec, NULL);
-	}
-
-	exit(1);
+	// timeout reached
+	return -1;
 }
 
 /*
  * Terminate all existing processes.
  */
 void
-reset_procs()
+smite()
 {
-	wx("waiting up to 10 seconds for processes to exit...");
+	notice("waiting up to 5 seconds for processes to exit...");
 
+	// send TERM 0.1 seconds before HUP
 	kill(-1, SIGTERM);
+	nanosleep(&(struct timespec){0,100000000}, NULL);
 	kill(-1, SIGHUP);
-	kill(-1, SIGCONT);
-	if (wait_for_upto(8) == 0)
+	if (wait_while_procs(5) == 0)
 		return;
 
+	notice("waiting up to 3 seconds for processes to force-exit...");
 	kill(-1, SIGKILL);
-	if (wait_for_upto(2) == 0)
+	if (wait_while_procs(3) == 0)
 		return;
 
-	wx("some processes would not die; ps axl advised");
+	warning("some processes would not die; ps axl advised");
 }
 
 /*
- * warn(3) and warnx(3) clones.
+ * Logging functions.
  * They open a new fd to the console to print to every time;
- * on BSD, it seems all existing file descriptors to a tty get yiffed if
+ * existing file descriptors to a tty get yiffed if
  * its controlling process exits.
  */
 void
-w(const char *fmt, ...)
+notice(const char *fmt, ...)
 {
 	va_list ap;
 	va_start(ap, fmt);
-
-	int fd = open(_PATH_CONSOLE, O_RDWR|O_NOCTTY);
-	dprintf(fd, "init: ");
-	vdprintf(fd, fmt, ap);
-	dprintf(fd, ": %s\n", strerror(errno));
-	close(fd);
-
+	l(LOG_NOTICE, fmt, ap);
 	va_end(ap);
 }
 void
-wx(const char *fmt, ...)
+warning(const char *fmt, ...)
 {
 	va_list ap;
 	va_start(ap, fmt);
-
-	int fd = open(_PATH_CONSOLE, O_RDWR|O_NOCTTY);
+	l(LOG_WARNING, fmt, ap);
+	va_end(ap);
+}
+void
+error(const char *fmt, ...)
+{
+	va_list ap;
+	va_start(ap, fmt);
+	l(LOG_ERR, fmt, ap);
+	va_end(ap);
+}
+void
+l(int priority, const char *fmt, va_list ap)
+{
+	int fd = open(_PATH_CONSOLE, O_RDWR|O_CLOEXEC|O_NOCTTY);
 	dprintf(fd, "init: ");
 	vdprintf(fd, fmt, ap);
 	dprintf(fd, "\n");
 	close(fd);
 
-	va_end(ap);
+	vsyslog(priority, fmt, ap);
 }
